2385b21efed4cb2a8c9574ade535347d0f74c988
miqiaoli <756994691@qq.com>
2018-07-27 08:47:39 +0800

    小八塑价已经最新价格修正

--- src/config.js
+++ src/config.js
@@ -1,13 +1,12 @@
 // const host = 'http://10.0.0.2/front' //  李成  黄页
-// const host = 'http://1891z163m0.iask.in/front' 
+// const host = 'http://1891z163m0.iask.in/front'
 const host = 'https://www.otimes.info/front' //测试环境
     // const host = 'https://www.otimes.com/front'
 
 // const imgHost = 'https://www.otimes.com' // 测试图片地址
 const imgHost = 'https://www.otimes.info'
-    // const socketHost = 'wss://www.otimes.info' // websocket地址
-const socketHost = 'wss://www.otimes.com'
-    // const socketHost = 'ws://192.168.3.121'
+const socketHost = 'wss://www.otimes.info' // websocket地址
+    // const socketHost = 'wss://www.otimes.com'
 
 export const __getApi = {
     host,
