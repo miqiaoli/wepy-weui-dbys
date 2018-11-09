457ef8a3c2ece107c1d72f869965e8c2e0fa5756
afacode <afacode@outlook.com>
2018-11-09 16:18:36 +0800

    修改域名

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
-const host = 'https://www.otimes.com/front' 
-// const host = 'https://www.otimes.info/front'
+// const host = 'https://www.otimes.com/front' 
+const host = 'https://dbys.otimes.info/front'
 
-// const imgHost = 'https://www.otimes.info' // 测试图片地址
-const imgHost = 'https://www.otimes.com'
-const socketHost = 'wss://www.otimes.com' // websocket地址
-// const socketHost = 'wss://www.otimes.info'
+const imgHost = 'https://dbys.otimes.info' // 测试图片地址
+// const imgHost = 'https://www.otimes.com'
+// const socketHost = 'wss://www.otimes.com' // websocket地址
+const socketHost = 'wss://dbys.otimes.info'
 
 export const __getApi = {
   host,
   imgHost,
   socketHost,
-  baseHrefUrl: 'https://www.otimes.com',
+  baseHrefUrl: 'https://dbys.otimes.com',
 
   //code encryptedData iv 换取openid，unionid,session_key
   _getPrivacyToken: `${host}/wxapp/codeei.dbys`,
