0ac977576ad8247f9822d2772463bbc678891eb8
afacode <afacode@outlook.com>
2019-08-27 17:46:16 +0800

    U: 联系方式，环球资源更新

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// const host = 'https://www.otimes.com/front'
-const host = 'https://dbys.otimes.info/front'
+const host = 'https://www.otimes.com/front'
+// const host = 'https://dbys.otimes.info/front'
 // const imgHost = 'https://dbys.otimes.info' // 测试图片地址
 const imgHost = 'https://www.otimes.com'
 const socketHost = 'wss://www.otimes.com' // websocket地址
