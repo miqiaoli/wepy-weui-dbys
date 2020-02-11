b174e387d070ddd9cd6ca18e0d0772cda962941a
afacode <afacode@outlook.com>
2020-02-11 17:48:11 +0800

    doc: 切换环境

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// const host = 'https://www.otimes.com/front'
-const host = 'http://dbys.otimes.com/front'
+const host = 'https://www.otimes.com/front'
+// const host = 'http://dbys.otimes.com/front'
 // const host = 'https://dbys.otimes.info/front'
 // const imgHost = 'https://dbys.otimes.info' // 测试图片地址
 const imgHost = 'https://www.otimes.com'
