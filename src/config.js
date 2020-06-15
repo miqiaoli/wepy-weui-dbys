fef80bde86586e27e28e8ebc8100f084935d716a
afacode <afacode@outlook.com>
2020-06-15 17:26:23 +0800

    fix: append

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// const host = 'https://www.otimes.com/front'
-const host = 'https://www.dbys.top/front'
+const host = 'https://www.otimes.com/front'
+// const host = 'https://www.dbys.top/front'
 // const imgHost = 'https://www.dbys.top' // 测试图片地址
 const imgHost = 'https://www.otimes.com'
 const socketHost = 'wss://www.otimes.com' // websocket地址
