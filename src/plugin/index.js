81174c11019e017bd1a017e19eaffbdb62fe33ed
afacode <afacode@outlook.com>
2020-07-16 17:09:47 +0800

    feat: 直播插件

---
+++ src/plugin/index.js
@@ -0,0 +1,21 @@
+const Sdk = require('./sdk/sdk.min.js')
+export const Vhall = {
+    Event: Sdk.Vhall.Event,
+    Player: Sdk.Vhall.Player,
+    VideoJs: Sdk.Vhall.VideoJs,
+    VhallMain: Sdk.Vhall.VhallMain,
+    DrawUtil: Sdk.Vhall.DrawUtil,
+    DocHelp: Sdk.Vhall.DocHelp,
+    Socket: Sdk.Vhall.Socket
+}
+export default {
+    play: Sdk.default.play,
+    seek: Sdk.default.seek,
+    pause: Sdk.default.pause,
+    fullScreen: Sdk.default.fullScreen,
+    exitFullScreen: Sdk.default.exitFullScreen,
+    changePlaySource:Sdk.default.changePlaySource,
+    setPlaybackRate: Sdk.default.playbackRate,
+    playbackRateList: Sdk.default.getPlaybackRate,
+    setOptions: Sdk.default.setOptions
+}
